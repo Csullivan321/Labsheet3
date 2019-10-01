02c61457d7e065c645e92fbc8ff07c91a45dba3b
s00188845 <s00188845@LAB-B1041-12>
2019-10-01 12:48:27 +0100

    added class

---
+++ Ex1/Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    public class Car
+    {
+        private string make;
+
+        public string Make
+        {
+            get { return make; }
+            set { make = value; }
+        }
+
+        private string model;
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        private string currentspd;
+
+        public string CurrentSpd
+        {
+            get { return currentspd; }
+            set { currentspd = value; }
+        }
+
+        private string enginesize;
+
+        public string EngineSize
+        {
+            get { return enginesize; }
+            set { enginesize = value; }
+        }
+
+    }
+}
